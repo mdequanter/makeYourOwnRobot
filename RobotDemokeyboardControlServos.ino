808c9b055067adbf4f8cea544d07db19d14daec4
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2021-11-29 07:09:15 +0100

    Update RobotDemokeyboardControlServos.ino

--- RobotDemokeyboardControlServos.ino
+++ RobotDemokeyboardControlServos.ino
@@ -1,3 +1,22 @@
+/*
+In this tutorial we will build a robot that consists of 4 individually proportionally controlled servo motors.
+
+Rotate Up and down Forward and backward Grab and release
+
+We use Arduino as a microcontroller management platform. Through our computer, we can control the robot via serial commands (USB port).
+
+The robot itself also has a recording and playback function so that repetitive tasks can be performed.
+
+Moreover, the step size can be adjusted so that we can work with millimeter accuracy.
+
+As hardware we use the technic blocks from the Heliwi.com construction kit.
+
+This code can be uploaded to your Arduino Uno
+
+#  Created by Maarten Dequanter (Erasmus Hogeschool Brussel) 29/11/2021
+
+*/
+
 #include <Servo.h>
 
 Servo angle;
